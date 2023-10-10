425911517e7880924e75cb9b0dcaebd292c1a972
Валерий Манойлов <manojlovv@inbox.ru>
2023-10-10 18:21:13 +0300

    fix: Game.py

--- StartCraftExample/Game.py
+++ StartCraftExample/Game.py
@@ -8,7 +8,7 @@ img_dir = path.join(path.dirname(__file__), 'img')
 
 WIDTH = 480
 HEIGHT = 600
-FPS = 30
+FPS = 40
 
 # Задаем цвета
 WHITE = (255, 255, 255)
@@ -39,9 +39,9 @@ class Player(pygame.sprite.Sprite):
         self.speedx = 0
         keystate = pygame.key.get_pressed()
         if keystate[pygame.K_LEFT]:
-            self.speedx = -8
+            self.speedx = -9
         if keystate[pygame.K_RIGHT]:
-            self.speedx = 8
+            self.speedx = 9
         self.rect.x += self.speedx
         if self.rect.right > WIDTH:
             self.rect.right = WIDTH
@@ -80,7 +80,7 @@ class Bullet(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.bottom = y
         self.rect.centerx = x
-        self.speedy = -30
+        self.speedy = -25
 
     def update(self):
         self.rect.y += self.speedy
